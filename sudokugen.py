97de171ee2ac1802667c938d1dbcb9a888679406
bv88 <bv88@drexel.edu>
2021-04-11 23:47:51 +0700

    update code format

--- sudokugen.py
+++ sudokugen.py
@@ -1,152 +1,165 @@
-from random import shuffle,sample,randint
+from random import shuffle, sample, randint
 
-def in_square(row,col):
-    if row<3 and col<3:
+
+def in_square(row, col):
+    if row < 3 and col < 3:
         return 1
-    elif col>=3 and col<6 and row<3:
+    elif col >= 3 and col < 6 and row < 3:
         return 2
-    elif col>=6 and col<9 and row<3:
+    elif col >= 6 and col < 9 and row < 3:
         return 3
-    elif col<3 and row>=3 and row<6:
+    elif col < 3 and row >= 3 and row < 6:
         return 4
-    elif col>=3 and col<6 and row>=3 and row<6:
+    elif col >= 3 and col < 6 and row >= 3 and row < 6:
         return 5
-    elif col>=6 and col<9 and row>=3 and row<6:
+    elif col >= 6 and col < 9 and row >= 3 and row < 6:
         return 6
-    elif col<3 and row>=6 and row<9:
+    elif col < 3 and row >= 6 and row < 9:
         return 7
-    elif col>=3 and col<6 and row>=6 and row<9:
+    elif col >= 3 and col < 6 and row >= 6 and row < 9:
         return 8
-    elif col>=6 and col<9 and row>=6 and row<9:
+    elif col >= 6 and col < 9 and row >= 6 and row < 9:
         return 9
     else:
         return 0
-def ite_square(board,row,col):
-    squ=[]
-    sub_square=in_square(row,col)
-    if sub_square==1:
-        for i in range(0,3):
-            for j in range(0,3):
-                squ.append((board[i][j],i,j))
+
+
+def ite_square(board, row, col):
+    squ = []
+    sub_square = in_square(row, col)
+    if sub_square == 1:
+        for i in range(0, 3):
+            for j in range(0, 3):
+                squ.append((board[i][j], i, j))
         return squ
-    elif sub_square==2:
-        for i in range(0,3):
-            for j in range(3,6):
-                squ.append((board[i][j],i,j))
-        return squ 
-    elif sub_square==3:
-        for i in range(0,3):
-            for j in range(6,9):
-                squ.append((board[i][j],i,j))
-        return squ 
-    elif sub_square==4:
-        for i in range(3,6):
-            for j in range(0,3):
-                squ.append((board[i][j],i,j))
-        return squ 
-    elif sub_square==5:
-        for i in range(3,6):
-            for j in range(3,6):
-                squ.append((board[i][j],i,j))
-        return squ 
-    elif sub_square==6:
-        for i in range(3,6):
-            for j in range(6,9):
-                squ.append((board[i][j],i,j))
-        return squ 
-    elif sub_square==7:
-        for i in range(6,9):
-            for j in range(0,3):
-                squ.append((board[i][j],i,j))
-        return squ 
-    elif sub_square==8:
-        for i in range(6,9):
-            for j in range(3,6):
-                squ.append((board[i][j],i,j))
-        return squ 
-    elif sub_square==9:
-        for i in range(6,9):
-            for j in range(6,9):
-                squ.append((board[i][j],i,j))
-        return squ 
-    elif sub_square==0:
+    elif sub_square == 2:
+        for i in range(0, 3):
+            for j in range(3, 6):
+                squ.append((board[i][j], i, j))
+        return squ
+    elif sub_square == 3:
+        for i in range(0, 3):
+            for j in range(6, 9):
+                squ.append((board[i][j], i, j))
+        return squ
+    elif sub_square == 4:
+        for i in range(3, 6):
+            for j in range(0, 3):
+                squ.append((board[i][j], i, j))
+        return squ
+    elif sub_square == 5:
+        for i in range(3, 6):
+            for j in range(3, 6):
+                squ.append((board[i][j], i, j))
+        return squ
+    elif sub_square == 6:
+        for i in range(3, 6):
+            for j in range(6, 9):
+                squ.append((board[i][j], i, j))
+        return squ
+    elif sub_square == 7:
+        for i in range(6, 9):
+            for j in range(0, 3):
+                squ.append((board[i][j], i, j))
+        return squ
+    elif sub_square == 8:
+        for i in range(6, 9):
+            for j in range(3, 6):
+                squ.append((board[i][j], i, j))
+        return squ
+    elif sub_square == 9:
+        for i in range(6, 9):
+            for j in range(6, 9):
+                squ.append((board[i][j], i, j))
+        return squ
+    elif sub_square == 0:
         return 0
-def valid_sudoku(board,row,col):
-    toggle=0
-    for i in range(0,9):
-        if board[i][col]==board[row][col]:
-            if i==row:
+
+
+def valid_sudoku(board, row, col):
+    toggle = 0
+    for i in range(0, 9):
+        if board[i][col] == board[row][col]:
+            if i == row:
                 continue
-            toggle+=1
+            toggle += 1
             break
-    for i in range(0,9):
-        if board[row][i]==board[row][col]:
-            if i==col:
+    for i in range(0, 9):
+        if board[row][i] == board[row][col]:
+            if i == col:
                 continue
-            toggle+=1
+            toggle += 1
             break
-    for k,i,j in ite_square(board,row,col):
-        if board[row][col]==k:
-            if i==row and j==col:
+    for k, i, j in ite_square(board, row, col):
+        if board[row][col] == k:
+            if i == row and j == col:
                 continue
-            toggle+=1
+            toggle += 1
             break
-    if toggle==0:
+    if toggle == 0:
         return True
     else:
         return False
-def sudoku_gen(board=[["-","-","-","-","-","-","-","-","-"],["-","-","-","-","-","-","-","-","-"],["-","-","-","-","-","-","-","-","-"],["-","-","-","-","-","-","-","-","-"],["-","-","-","-","-","-","-","-","-"],["-","-","-","-","-","-","-","-","-"],["-","-","-","-","-","-","-","-","-"],["-","-","-","-","-","-","-","-","-"],["-","-","-","-","-","-","-","-","-"]]):
-   #board size is 9x9 by default
-    nlist=[1,2,3,4,5,6,7,8,9]
+
+
+def sudoku_gen(board=[["-", "-", "-", "-", "-", "-", "-", "-", "-"], ["-", "-", "-", "-", "-", "-", "-", "-", "-"], ["-", "-", "-", "-", "-", "-", "-", "-", "-"], ["-", "-", "-", "-", "-", "-", "-", "-", "-"], ["-", "-", "-", "-", "-", "-", "-", "-", "-"], ["-", "-", "-", "-", "-", "-", "-", "-", "-"], ["-", "-", "-", "-", "-", "-", "-", "-", "-"], ["-", "-", "-", "-", "-", "-", "-", "-", "-"], ["-", "-", "-", "-", "-", "-", "-", "-", "-"]]):
+   # board size is 9x9 by default
+    nlist = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     shuffle(nlist)
-    solve_sudoku(board,0,0,nlist)
-    winboard=board.copy()
+    solve_sudoku(board, 0, 0, nlist)
+    winboard = board.copy()
     for i in range(9):
-        winboard[i]=board[i].copy()
-    for row in range(0,9):
-        space=sample(range(0,9),randint(5,6))
-        for col in range(0,9):
+        winboard[i] = board[i].copy()
+    for row in range(0, 9):
+        space = sample(range(0, 9), randint(5, 6))
+        for col in range(0, 9):
             if col in space:
-                board[row][col]='-'              
-    return board,winboard
-def check_board(board,winboard):
-    wrong=[]
+                board[row][col] = '-'
+    return board, winboard
+
+
+def check_board(board, winboard):
+    wrong = []
     for i in range(9):
         for j in range(9):
-            if board[i][j]!=winboard[i][j]:
-                wrong.append((board[i][j],i,j))
+            if board[i][j] != winboard[i][j]:
+                wrong.append((board[i][j], i, j))
     return wrong
+
+
 def locked_tab(board):
-    const=[]
-    for i in range (0,9):
+    const = []
+    for i in range(0, 9):
         for j in range(9):
-            if board[i][j]!='-':
-                const.append((i,j))
+            if board[i][j] != '-':
+                const.append((i, j))
     return const
-def solve_sudoku(board,row,col,listt=[1,2,3,4,5,6,7,8,9]):
-    if (row,col) in locked_tab(board):
-        return solve_sudoku(board,row,col+1,listt)
-    if col>8:
-        return solve_sudoku(board,row+1,0,listt)
-    if row>8 :
+
+
+def solve_sudoku(board, row, col, listt=[1, 2, 3, 4, 5, 6, 7, 8, 9]):
+    if (row, col) in locked_tab(board):
+        return solve_sudoku(board, row, col+1, listt)
+    if col > 8:
+        return solve_sudoku(board, row+1, 0, listt)
+    if row > 8:
         return True
     else:
         for k in listt:
-            board[row][col]=k
-            if valid_sudoku(board,row,col):
-                if solve_sudoku(board,row,col+1,listt):
+            board[row][col] = k
+            if valid_sudoku(board, row, col):
+                if solve_sudoku(board, row, col+1, listt):
                     return True
-        board[row][col]='-'
+        board[row][col] = '-'
         return False
 
-            
-if __name__=="__main__":
 
-    const=[]
-    board,winboard=sudoku_gen()
+if __name__ == "__main__":
+
+    const = []
+    board, winboard = sudoku_gen()
     for i in board:
         print(i)
     print("\n")
-    solve_sudoku(board,0,0)
+    solve_sudoku(board, 0, 0)
     for i in board:
         print(i)
